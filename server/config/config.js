cb30793664fba5623330360b11fd4f8d5beb25d5
Edmundo <correoedmundo@gmail.com>
2020-12-29 22:14:14 -0600

    Primera prueba

--- server/config/config.js
+++ server/config/config.js
@@ -5,8 +5,8 @@ process.env.PORT = process.env.PORT || 3000;
 let urldb;
 
 if (process.env.NODE_ENV === 'dev') {
-    //urldb = 'mongodb://localhost:27017/cuestionario';
-    urldb = 'mongodb+srv://edmundolopez:I6spJ1keq04Y3lxy@cluster0.fv69p.mongodb.net/cuestionario';
+    urldb = 'mongodb://localhost:27017/cuestionario';
+    //urldb = 'mongodb+srv://edmundolopez:I6spJ1keq04Y3lxy@cluster0.fv69p.mongodb.net/cuestionario';
 } else {
     urldb = process.env.MONGO_URI;
 }
