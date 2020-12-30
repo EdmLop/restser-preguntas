3f8cb06d80cf9dd3f4fd3ff9f014582a6df024a9
Edmundo <correoedmundo@gmail.com>
2020-12-30 01:37:13 -0600

    Quitar bd de la config

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,8 @@ let urldb;
 
 if (process.env.NODE_ENV === 'dev') {
     urldb = 'mongodb://localhost:27017/cuestionario';
-    //urldb = 'mongodb+srv://edmundolopez:I6spJ1keq04Y3lxy@cluster0.fv69p.mongodb.net/cuestionario';
 } else {
     urldb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urldb;
-
-//I6spJ1keq04Y3lxy
